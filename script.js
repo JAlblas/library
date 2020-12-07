1b428b1d561aaaf880afcc0cb2421cb1015e7f2f
Jasper Alblas <just_jasper@hotmail.com>
2020-12-07 15:10:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "has read" : "not read"}`
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    let bookListDiv = document.querySelector('#book-list');
+
+    myLibrary.forEach(book => {
+
+        console.log(book.info());
+
+        const content = document.createElement('div');
+        content.classList.add('card');
+        content.classList.add('bg-info');
+        content.classList.add('text-white');
+        content.style.width = '18rem';
+
+        const innerContent = document.createElement('div');
+        innerContent.classList.add('card-body');
+
+        const header = document.createElement('h5');
+        header.classList.add('card-title');
+        header.textContent = book.title;
+
+        const paragraph = document.createElement('p');
+        paragraph.classList.add('card-text');
+        paragraph.textContent = book.info();
+
+        innerContent.appendChild(header);
+        innerContent.appendChild(paragraph);
+
+        content.appendChild(innerContent);
+        bookListDiv.appendChild(content);
+    });
+}
+
+let book1 = new Book("Game of Thrones", "G.R.R.Martin", 942, true);
+addBookToLibrary(book1);
+
+let book2 = new Book("Game of Thrones", "G.R.R.Martin", 942, true);
+addBookToLibrary(book2);
+
+let book3 = new Book("Game of Thrones", "G.R.R.Martin", 942, true);
+addBookToLibrary(book3);
+
+let book4 = new Book("Game of Thrones", "G.R.R.Martin", 942, true);
+addBookToLibrary(book4);
+
+displayBooks();
+
+
